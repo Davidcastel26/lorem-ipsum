903bbfc60a48b557e29cc0ecede6e8a9329e70ea
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-05 18:02:31 -0600

    done with lorem ipsum

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import text from "./data";
 
 const App = () => {
   
   const [count, setCount] = useState(1);
-  const [text, setText] = useState([]);
+  const [dataText, setDataText] = useState([]);
 
   const handleSubmit = ( e ) => {
     e.preventDefault()
     let amount = parseInt(count)
-    
+    setDataText( text.slice(0, amount) )
   }
 
   return (
@@ -29,6 +30,13 @@ const App = () => {
         />
         <button className="btn" type="submit">generate</button>
       </form>
+      <article className="lorem-text">
+        {
+          dataText.map((item) => {
+            return <p key={nanoid()}>{item}</p>
+          })
+        }
+      </article>
     </section>
   )
 };
